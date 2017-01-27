de48101772de6d8de79b23940df5871e3fdb3552
Z1374K <Z1374K@gmail.com>
2017-01-27 17:44:08 +0100

    navigation modified, post.php added and filled with content

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,22 +1,17 @@
 <?php
 
 if(isset($_POST['create_post'])){
-    echo $_POST['create_post'];
-
 
     $post_title = $_POST['post_title'];
     $post_author = $_POST['post_author'];
-    $post_category_id = $_POST['post_category_id'];
+    $post_category_id = $_POST['post_category'];
     $post_status = $_POST['post_status'];
-
     $post_image = $_FILES['image']['name'];
     $post_image_temp = $_FILES['image']['tmp_name'];
-
     $post_tags = $_POST['post_tags'];
     $post_content = $_POST['post_content'];
     $post_date = date('d-m-y');
     $post_comment_count = 4;
-
 
     move_uploaded_file($post_image_temp, "../images/$post_image");
 
@@ -25,7 +20,7 @@ if(isset($_POST['create_post'])){
 
 $create_post_query = mysqli_query($conn, $query);
 
-confirmQuery($create_post_query);
+confirmQuery($create_post_query . "fu***d up here");
 
 }
 
@@ -37,8 +32,18 @@ confirmQuery($create_post_query);
         <input type="text" class="form-control" name="post_title">
     </div>
     <div class="form-group">
-        <label for="post_category">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <select name="post_category" id="">
+            <?php
+            $query ="SELECT * FROM categories";
+            $select_categories = mysqli_query($conn, $query);
+            confirmQuery($select_categories);
+            while ($row = mysqli_fetch_assoc($select_categories)) {
+                $cat_id = $row['cat_id'];
+                $cat_title = $row['cat_title'];
+                echo "<option value='$cat_id'>{$cat_title}</option>";
+            }
+            ?>
+        </select>
     </div>
     <div class="form-group">
         <label for="author">Post Author</label>
